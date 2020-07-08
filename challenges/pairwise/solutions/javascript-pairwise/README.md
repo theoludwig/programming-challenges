60bb10be6e3bdb600773860d2933e715e3a0d19a
divlo <contact@divlo.fr>
2020-07-08 19:56:30 +0200

    ✨ Add "pairwise" challenge

---
+++ challenges/pairwise/solutions/javascript-pairwise/README.md
@@ -0,0 +1,4 @@
+# javascript-pairwise - pairwise
+
+Programming language : JavaScript
+Created by [@Divlo](https://github.com/Divlo) at 8 July 2020.
---
+++ challenges/pairwise/solutions/javascript-pairwise/solution.js
@@ -0,0 +1,15 @@
+function solution (arr, arg) {
+  return arr.reduce((accumulator, currentNumber, indexReduce, array) => {
+    for (let index = indexReduce + 1; index < array.length; index++) {
+      if (array[index] + array[indexReduce] === arg) {
+        accumulator += indexReduce + index
+        array[indexReduce] = NaN
+        array[index] = NaN
+        break
+      }
+    }
+    return accumulator
+  }, 0)
+}
+
+module.exports = solution
