5a1a32a8f614fe2d9cc1419864eb3a66bbb4c968
divlo <contact@divlo.fr>
2021-06-07 19:01:00 +0200

    chore: should fix ci

--- templates/solution/java/Solution.java
+++ templates/solution/java/Solution.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Solution {
   public static void main(String[] args) {
     String line;
-		Scanner scanner = new Scanner(System.in);
-		while(scanner.hasNextLine()) {
-			line = scanner.nextLine();
+    Scanner scanner = new Scanner(System.in);
+    while(scanner.hasNextLine()) {
+      line = scanner.nextLine();
       System.out.println("Hello, " + line + "!");
-		}
+    }
   }
 }
