d45453e00a6cb6331085bff5cae3641eeb2a174a
Divlo <contact@divlo.fr>
2021-06-29 20:38:10 +0200

    feat(solutions): add `sorting-algorithms/python/bubble-sort`

---
+++ challenges/sorting-algorithms/solutions/python/bubble-sort/solution.py
@@ -0,0 +1,23 @@
+from typing import List, Any
+import sys
+
+
+def bubble_sort(numbersInput: List[int]) -> List[int]:
+    numbers = list(numbersInput)
+    length = len(numbers)
+    for index_1 in range(length):
+        for index_2 in range(length - index_1 - 1):
+            if numbers[index_2] > numbers[index_2 + 1]:
+                temporary = numbers[index_2]
+                numbers[index_2] = numbers[index_2 + 1]
+                numbers[index_2 + 1] = temporary
+    return numbers
+
+
+numbers: List[int] = []
+for value in sys.stdin:
+    numbers.append(int(value.rstrip('\n')))
+
+sorted_numbers = bubble_sort(numbers)
+for number in sorted_numbers:
+    print(number)
