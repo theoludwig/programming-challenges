aa3eec06baf44a59b4899208649bef2ebe0acf66
Divlo <contact@divlo.fr>
2021-06-29 22:12:13 +0200

    feat(solutions): add `sorting-algorithms/c/bubble-sort`

---
+++ challenges/sorting-algorithms/solutions/c/bubble-sort/bubble_sort.h
@@ -0,0 +1,8 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
+
+void swap_numbers(int *value_1, int *value_2);
+
+void bubble_sort(int *numbers, const int length);
+
+#endif
---
+++ challenges/sorting-algorithms/solutions/c/bubble-sort/solution.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bubble_sort.h"
+
+int main() {
+  int *numbers = malloc(25000 * sizeof(int));
+  int current_number;
+  int index_input = 0;
+  while (scanf("%d", &current_number) != EOF) {
+    numbers[index_input] = current_number;
+    index_input += 1;
+  }
+  bubble_sort(numbers, index_input);
+  for (int index = 0; index < index_input; index++) {
+    printf("%d\n", numbers[index]);
+  }
+  free(numbers);
+  return 0;
+}
+
+void bubble_sort(int numbers[], const int length) {
+  for (int index_1 = 0; index_1 < length; index_1++) {
+    for (int index_2 = 0; index_2 < length - index_1 - 1; index_2++) {
+      if (numbers[index_2] > numbers[index_2 + 1]) {
+        swap_numbers(&numbers[index_2], &numbers[index_2 + 1]);
+      }
+    }
+  }
+}
+
+void swap_numbers(int *value_1, int *value_2) {
+  int temporary = *value_1;
+  *value_1 = *value_2;
+  *value_2 = temporary;
+}
