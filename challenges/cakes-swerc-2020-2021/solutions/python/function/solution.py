0df7b6620eb5d71462d3118e68191dc557cf7c70
Divlo <contact@divlo.fr>
2022-04-23 20:52:00 +0200

    feat(solutions): add `cakes-swerc-2020-2021/python/function`

---
+++ challenges/cakes-swerc-2020-2021/solutions/python/function/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+import sys
+
+maximum_number_of_cake_possible = None
+
+next(sys.stdin)
+for line in sys.stdin:
+    quantity_per_cake, quantity_available = list(map(int, line.split(' ')))
+    cake_possible = quantity_available // quantity_per_cake
+    if maximum_number_of_cake_possible == None or cake_possible < maximum_number_of_cake_possible:
+        maximum_number_of_cake_possible = cake_possible
+
+print(maximum_number_of_cake_possible)
