a9dc21f0db238264e8ea5c17b8056a55d809b0d5
Divlo <contact@divlo.fr>
2022-04-23 17:33:45 +0200

    fix: update Dockerfile to latest versions

---
+++ challenges/sorting-algorithms/solutions/python/bubble-sort/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+import sys
+
+
+def bubble_sort(numbersInput: List[int]) -> List[int]:
+    numbers = list(numbersInput)
+    length = len(numbers)
+    for index_1 in range(length):
+        for index_2 in range(length - index_1 - 1):
+            if numbers[index_2] > numbers[index_2 + 1]:
+                temporary = numbers[index_2]
+                numbers[index_2] = numbers[index_2 + 1]
+                numbers[index_2 + 1] = temporary
+    return numbers
+
+
+numbers: List[int] = []
+for value in sys.stdin:
+    numbers.append(int(value.rstrip('\n')))
+
+numbers = numbers[1:]
+sorted_numbers = bubble_sort(numbers)
+for number in sorted_numbers:
+    print(number)
